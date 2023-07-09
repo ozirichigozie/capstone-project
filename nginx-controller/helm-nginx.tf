a90e081340c61d589d0e593b9119f221c39edea8
Chigozie Oziri <blessedchigozie22@gmail.com>
2023-07-09 20:33:42 +0100

    changed k8_namespace to lakesidemutual

--- nginx-controller/helm-nginx.tf
+++ nginx-controller/helm-nginx.tf
@@ -11,7 +11,8 @@ resource "kubernetes_namespace" "nginx-namespace" {
 
   depends_on = [time_sleep.wait_for_kubernetes]
   metadata {
-    name = "nginx-ingress"
+    name = "lakesidemutual"
+    # formerly nginx-ingress
   }
 }
 
